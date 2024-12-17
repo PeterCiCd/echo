f76b2feb0b398fb638ba24402eb81c3e3f6c7eb1
HB <HB@email.mail>
2024-12-17 22:36:26 +0300

    init

--- src/main/java/ru/echo/echo/controller/cargo.java
+++ src/main/java/ru/echo/echo/controller/cargo.java
@@ -1,20 +1,17 @@
 package ru.echo.echo.controller;
 
-import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@Slf4j
 @RestController
 public class cargo {
 
     @PostMapping("/route")
     public String checkRoute(@RequestBody String string){
 
-//      log.info("Получено {}", string);
         return string;
     }
 }
